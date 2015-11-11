a174a20f28f27b43dde870f16d62232c820a6de2
AndreiB <bocsan_andrei@yahoo.com>
2015-11-11 13:09:10 +0200

    update

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -22,6 +22,7 @@ class User extends Model implements AuthenticatableContract,
      * @var string
      */
     protected $table = 'users';
+    protected $primaryKey = 'user_id';
 
     /**
      * The attributes that are mass assignable.
