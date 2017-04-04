437ec4a7b4f41359f5d338d27c488208d65bc940
BoAndrei <bocsan.andrei@gmail.com>
2017-04-04 15:17:38 +0300

    Laravel Project

--- project_laravel/resources/views/TableBody.blade.php
+++
@@ -1,4 +0,0 @@
- <td>{{ $product->product_name }}</td>
-    <td>{{ $product->product_description }}</td>
-    <td>{{ $product->product_price }}</td>
-    <td><image height="100" width="100" src="/images/{{ $product->product_image }}" /></td>
