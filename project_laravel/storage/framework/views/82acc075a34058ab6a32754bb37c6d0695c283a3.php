ec871f242724cf5f9e678a255a032e677369e782
BoAndrei <bocsan.andrei@gmail.com>
2017-04-18 13:30:48 +0300

    Project Concrete

--- project_laravel/storage/framework/views/82acc075a34058ab6a32754bb37c6d0695c283a3.php
+++
@@ -1,7 +0,0 @@
-<tr>
-    <th>Name</th>
-    <th>Description</th>
-    <th>Price</th>
-    <th>Image</th>
-    <th>Operation</th>
-</tr>
--- project_laravel/storage/framework/views/8fe757af795287ea3bb2d3c429e6a4d5dfdfb38b.php
+++
@@ -1,5 +0,0 @@
-<td><?php echo e($product->name); ?></td>
-<td><?php echo e($product->description); ?></td>
-<td><?php echo e($product->price); ?></td>
-<td><image height="100" width="100" src="/images/<?php echo e($product->image); ?>" /></td>
-
